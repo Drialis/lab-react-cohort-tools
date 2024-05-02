47b262635ff85ac1e9028b0e82a86ad0a99afac1
Paloma Rehecho <paloma_re@hotmail.com>
2024-05-02 12:24:49 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import StudentDetailsPage from "./pages/StudentDetailsPage";
-import UserProfilePage from "./pages/UserProfilePage";
+import "./App.css"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import StudentDetailsPage from "./pages/StudentDetailsPage"
+import UserProfilePage from "./pages/UserProfilePage"
+import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
@@ -12,15 +13,17 @@ function App() {
       <Navbar />
 
       <div className="pages">
-        <HomePage />
 
-        <StudentDetailsPage />
+        <Routes>
+          <Route path={'/'} element={<HomePage />} />
+          <Route path={'/students/:studentId'} element={<StudentDetailsPage />} />
+          <Route path={'/profile'} element={<UserProfilePage />} />
+        </Routes>
 
-        <UserProfilePage />
       </div>
 
     </div>
-  );
+  )
 }
 
 export default App;
